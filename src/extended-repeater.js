6621ad18541de195301e8e711c6374d1c492231f
Simon <horselvaren@gmail.com>
2023-04-17 18:45:23 +0600

    feat: submit extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let
+      result = [],
+      stringAdd = []
+  
+  const
+      repeatTimes = options.repeatTimes === undefined ? 1 : options.repeatTimes,
+      separator = options.separator === undefined ? '+' : options.separator,
+      
+      addition = options.addition === undefined ? '' : options.addition,
+      additionSeparator = options.additionSeparator === undefined ? '|' : options.additionSeparator,
+      additionRepeatTimes = options.additionRepeatTimes === undefined ? 1 : options.additionRepeatTimes
+
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    stringAdd.push(`${addition}`)
+  }
+  
+  for (let i = 0; i < repeatTimes; i++) {
+    result.push(`${str}${stringAdd.join(additionSeparator)}`)
+  }
+  
+  return result.join(separator)
 }
 
 module.exports = {
   repeater
-};
+}
